ae2aadfccc65c42d97c9eab5174c2512fcffdc82
Bao Dang <baochidangg@gmail.com>
2024-11-13 01:22:43 -0500

    New start menu UI

---
+++ Assets/Scripts/Scene Change/StartMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class StartMenu : MonoBehaviour
+{
+  private Button playButton;
+  private TextField nameInput;
+
+  void OnEnable()
+  {
+    var root = GetComponent<UIDocument>().rootVisualElement;
+
+    nameInput = root.Q<TextField>("NameInput");
+    playButton = root.Q<Button>("PlayButton");
+
+    playButton.RegisterCallback<ClickEvent>(GoToLevel1);
+  
+  }
+
+  private void GoToLevel1(ClickEvent e)
+  {
+    Debug.Log("Player Name: " + nameInput.value);
+    SceneManager.LoadScene("Level1");
+  }
+}
