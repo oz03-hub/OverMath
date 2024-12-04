4e752ae934fc7679b7e20e3511640dec21155a08
Ozel Yilmazel <ozel@yilmazel.com>
2024-12-04 10:10:58 -0500

    comment out print

--- Assets/Scripts/AdversarialAudioManager.cs
+++ Assets/Scripts/AdversarialAudioManager.cs
@@ -21,7 +21,7 @@ public class AdversarialAudioManager : MonoBehaviour
         if (Time.time - lastBae >= 8f)
         {
             audioSource.PlayOneShot(BaeClip);
-            Debug.Log("Play BAEEE");
+            //Debug.Log("Play BAEEE");
 
             lastBae = Time.time;
         }
